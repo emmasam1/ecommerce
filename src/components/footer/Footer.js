24050bb075c74c83a3d3baa200088aa902754df3
oyis12 <oyivosam12@gmail.com>
2024-07-18 09:42:41 +0100

    project structured

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css"
+
+const Footer = () => {
+  return (
+    <div className={styles.footer_container}>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,9 @@
+import styles from "./navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={styles.nav_container}>Navbar</div>
+  )
+}
+
+export default Navbar
